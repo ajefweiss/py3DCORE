d96869c659db4ce1e231b3a05c561f3f37bec752
Weiss <andreas.weiss@oeaw.ac.at>
2020-10-30 11:11:08 +0100

    final 1.1.3

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ from setuptools import setup, find_packages
 setup(
     name="3DCORE",
     packages=find_packages(),
+    package_data={'': ['*.json']},
+    include_package_data=True,
     version="1.1.3",
     author="Andreas J. Weiss",
     author_email="andreas.weiss@oeaw.ac.at",
@@ -13,7 +15,7 @@ setup(
     keywords=["astrophysics", "solar physics", "space weather"],
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/ajefweiss/3DCORE",
+    url="https://github.com/ajefweiss/py3DCORE",
     install_requires=[
         "heliosat",
         "numba",
