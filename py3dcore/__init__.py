6eb55dd3007a3074ab48b1b62ab50a348e6ae9b6
Andreas J. Weiss <ajefweiss@gmail.com>
2023-02-14 16:15:24 -0500

    legacy fix

--- py3dcore/__init__.py
+++ py3dcore/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from py3dcore.methods import ABC_SMC
+from py3dcore.models import ToroidalModel
 
 __author__ = "Andreas J. Weiss"
 __copyright__ = "Copyright (C) 2019 Andreas J. Weiss"
